f46add86855ced5e4088caca99c53c22cc612009
fox fux <1492512300@qq.com>
2020-08-13 16:10:05 +0800

    source/lib/app_log:模板化

--- source/lib/app_log.h
+++ source/lib/app_log.h
@@ -1,102 +1,112 @@
 /*******************************************************************************
- *
- * Module: log
+ * LICENSE : Apache 2.0
  *           
  * History:
  *    <author>         <time>             <version>             <desc>
- *      FuxFox          2019/10/22 10:51          V1.0             build this file
+ *      FuxFox	      2020/08/13 16:01          V1.0             build this file
  *
  *******************************************************************************/
-/*!
- * \file     log.h
- * \author   FuxFox
- * \version  V1.0
- * \date       2019/10/22
- *******************************************************************************/
-#ifndef LOG_H
-#define LOG_H
+#ifndef APP_LOG_H
+#define APP_LOG_H
 
-/*! \defgroup log_public Public
-\ingroup log
-\brief            simple LOG module , used SEGGER RTT
+/*! \defgroup app_log
+\brief		simple LOG module
 \details 
 * @{ */
 
 #include "app_config.h"
-//********************************* Module Config *******************************/
-                                
-#define APP_LOG_CHANNEL_DEF 0
 
+//********************************* Module Config *******************************/
+
+#define APP_LOG_ENABLE		CFG_LOG_ENABLE
+#define APP_LOG_LEVEL		CFG_LOG_LEVEL
+
+#define APP_LOG_CHANNEL_DEF	CFG_LOG_CHANNEL_DEF
+
+#define APP_LOG_MODULE_SYMBLE_ENABLE	CFG_LOG_MODULE_SYMBLE_ENABLE
 #ifndef APP_LOG_MODULE_SYMBLE
 #define APP_LOG_MODULE_SYMBLE "USER"
 #endif
 //********************************* Data Type ***********************************/
 
-/*! \defgroup APP_LOG_LEVEL_TYPE
-*
-* @{ */
-#define APP_LOG_LEVEL_NONE         0
-#define APP_LOG_LEVEL_INFO         1
-#define APP_LOG_LEVEL_ERROR        2
-#define APP_LOG_LEVEL_WARNING      3
-#define APP_LOG_LEVEL_DEBUG        4
-
-/*! @}*/ //end of group APP_LOG_LEVEL_TYPE
-
-/*! log type enum */
 typedef enum
 {
-    LOG_TYPE_NONE,
-    LOG_TYPE_INFO,
-    LOG_TYPE_ERROR,
-    LOG_TYPE_WARNING,
-    LOG_TYPE_DEBUG            
-}app_log_type_t;
+	APP_LOG_LEVEL_NONE,
+	APP_LOG_LEVEL_INFO,
+	APP_LOG_LEVEL_ERROR,
+	APP_LOG_LEVEL_WARNING,
+	APP_LOG_LEVEL_DEBUG
+}app_log_level_t;
 
 //********************************* Public Interface ****************************/
+
+/*!*****************************************************************************
+\brief  	initialize UART or other channel to print log
+\param[in]	void
+\return     void
+******************************************************************************/
+#if (APP_LOG_ENABLE && APP_LOG_LEVEL)
 void app_log_init(void);
-void app_log_output(uint8_t channel, app_log_type_t type, const char* module_symble, const char* sformat, ...);
-void app_log_print_array_to_hex(uint8_t channel, uint8_t* arry, uint8_t size);
+#else
+#define app_log_init()
+#endif
 
-#define LOG(log_type, sformat, ...) app_log_output(APP_LOG_CHANNEL_DEF, log_type, APP_LOG_MODULE_SYMBLE, sformat, ##__VA_ARGS__)
+/*!*****************************************************************************
+\brief  	Print an array to Hex
+\param[in]	uint8_t * str : prompt information
+\param[in]	uint8_t * arry
+\param[in]	uint8_t size
+\return     void
+******************************************************************************/
+void app_log_print_array_to_hex(uint8_t* str, uint8_t* arry, uint8_t size);
 
+#if APP_LOG_MODULE_SYMBLE_ENABLE
+#define LOG(log_type, sformat, ...) print(#log_type APP_LOG_MODULE_SYMBLE sformat "\r\n", ##__VA_ARGS__)
+#else
+#define LOG(log_type, sformat, ...) print(#log_type sformat "\r\n", ##__VA_ARGS__)
+#endif
 
+#if APP_LOG_ENABLE
 #if (APP_LOG_LEVEL >= APP_LOG_LEVEL_INFO)
-#define LOG_INFO(sformat, ...) LOG(LOG_TYPE_INFO, sformat, ##__VA_ARGS__)
+#define APP_LOG_INFO(sformat, ...) LOG(INFO, sformat, ##__VA_ARGS__)
 #endif
 
 #if (APP_LOG_LEVEL >= APP_LOG_LEVEL_ERROR)
-#define LOG_ERROR(sformat, ...) LOG(LOG_TYPE_WARNING, sformat, ##__VA_ARGS__)
+#define APP_LOG_ERROR(sformat, ...) LOG(ERROR, sformat, ##__VA_ARGS__)
 #endif
 
 #if (APP_LOG_LEVEL >= APP_LOG_LEVEL_WARNING)
-#define LOG_WARNING(sformat, ...) LOG(LOG_TYPE_ERROR, sformat, ##__VA_ARGS__)
+#define APP_LOG_WARNING(sformat, ...) LOG(WARNING, sformat, ##__VA_ARGS__)
 #endif
 
 #if (APP_LOG_LEVEL >= APP_LOG_LEVEL_DEBUG)
-#define LOG_DEBUG(sformat, ...)  LOG(LOG_TYPE_DEBUG, sformat, ##__VA_ARGS__)
-#define LOG_DEBUG_ARRAY(arr, size) app_log_print_array_to_hex(APP_LOG_CHANNEL_DEF, arr, size);
+#define APP_LOG_DEBUG(sformat, ...)  LOG(DEBUG, sformat, ##__VA_ARGS__)
+#define APP_LOG_DEBUG_ARRAY(_str, _arr, _size) app_log_print_array_to_hex((uint8_t*)_str, _arr, _size);
+#endif
+#else
+
+#ifndef APP_LOG_INFO
+#define APP_LOG_INFO(sformat, ...) 
+#endif
+#ifndef APP_LOG_WARNING
+#define APP_LOG_WARNING(sformat, ...) 
+#endif
+#ifndef APP_LOG_ERROR
+#define APP_LOG_ERROR(sformat, ...) 
+#endif
+#ifndef APP_LOG_DEBUG
+#define APP_LOG_DEBUG(sformat, ...) 
+#define APP_LOG_DEBUG_ARRAY(_str, _arr, _size)
+#endif
 #endif
 
-#ifndef LOG_INFO
-#define LOG_INFO(sformat, ...) 
-#endif
-#ifndef LOG_WARNING
-#define LOG_WARNING(sformat, ...) 
-#endif
-#ifndef LOG_ERROR
-#define LOG_ERROR(sformat, ...) 
-#endif
-#ifndef LOG_DEBUG
-#define LOG_DEBUG(sformat, ...) 
-#define LOG_DEBUG_ARRAY(arr, size)
-#endif
-
-//********************************* Private Interface ***************************/
 
 
 
 
 
-/*! @}*/ //end of group log
-#endif // LOG_H
+
+
+
+/*! @}*/ //end of group app_log
+#endif // APP_LOG_H
