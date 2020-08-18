567536f399e70ab15a2a643ad9a6f7e724723763
fox fux <1492512300@qq.com>
2020-08-18 09:58:25 +0800

    docs: source/hal/* : 注释文档规范化

--- source/hal/hal_delay.h
+++ source/hal/hal_delay.h
@@ -1,18 +1,19 @@
 /*******************************************************************************
- * LICENSE : Apache 2.0
- *           
- * History:
- *    <author>         <time>             <version>             <desc>
- *      FuxFox	      2020/06/23 15:11          V1.0             build this file
- *
- *******************************************************************************/
+* LICENSE : Apache 2.0
+*
+* History:
+*    <author>         <time>             <version>             <desc>
+*      FuxFox	      2020/06/23 15:11          V1.0             build this file
+*
+*******************************************************************************/
 #ifndef HAL_DELAY_H
 #define HAL_DELAY_H
 
 /** @defgroup hal_delay
- * @brief Blocking-Delay HAL Template
- * @details 
- * @{ */
+* @ingroup HAL
+* @brief Blocking-Delay HAL Template
+* @details 
+* @{ */
 
 #include "app_config.h"
 
@@ -33,23 +34,23 @@
 
 
 /*!*****************************************************************************
-@brief      initialize the soft delay function
-            call this function after system clock initialization.
-@return     void
+* @brief      initialize the soft delay function
+*             call this function after system clock initialization.
+* @return     void
 *******************************************************************************/
 void hal_delay_init(void);
 
 /*!*****************************************************************************
-@brief      blocking delay in us
-@param[in]    uint32_t nus
-@return     void
+* @brief      blocking delay in us
+* @param[in]    uint32_t nus
+* @return     void
 *******************************************************************************/
 void hal_delay_us(uint32_t nus);
 
 /*!*****************************************************************************
-@brief      blocking delay in ms
-@param[in]    uint32_t nms
-@return     void
+* @brief      blocking delay in ms
+* @param[in]    uint32_t nms
+* @return     void
 *******************************************************************************/
 void hal_delay_ms(uint32_t nms);
 
